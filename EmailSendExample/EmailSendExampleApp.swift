33becc347ff8ca029df6ad9d8a130f2758ebe107
Russell Gordon <rgordon@lcs.on.ca>
2021-03-29 11:55:25 -0400

    Initial Commit

---
+++ EmailSendExample/EmailSendExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmailSendExampleApp.swift
+//  EmailSendExample
+//
+//  Created by Russell Gordon on 2021-03-29.
+//
+
+import SwiftUI
+
+@main
+struct EmailSendExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
